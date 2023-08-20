d5f82391c3af0e4a84a5c2b7cbc67b80471df5c4
banklessSoul <142628476+banklessSoul@users.noreply.github.com>
2023-08-20 01:32:40 -0700

    Create asset.js

---
+++ asset.js
@@ -0,0 +1,77 @@
+function openNav() {
+  document.getElementById("nav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("nav").style.width = "0";
+}
+
+function addDimension(svg, d) {
+    if (d.dimensionType != "timeline") {
+        svg.append("rect")
+            .attr("id", d.dimension)
+            .attr("class", "dimension")
+            .attr("x", d.x - d.w / 2)
+            .attr("y", d.y - d.h / 2)
+            .attr("width", d.w)
+            .attr("height", d.h)
+            .attr("rx", 10)
+            .style("fill", d.color);
+    } else {
+        addPath(svg, d)
+    }
+}
+
+function addPath(svg, d) {
+    const path = d3.path();
+    path.arc(d.x+d.xOff, d.y-d.yOff, d.r, 0, 2 * Math.PI);
+    svg
+        .append("path")
+		.attr("id", d.dimension)
+        .attr("class", "dimension")
+        .attr("d", path)
+        .attr("stroke", "black")
+        .attr("fill", d.color);
+}
+
+d3.json("observations.json", function(error, data) {
+    var imgBounds = d3.select('#mg').node().getBoundingClientRect();
+    var varnish = d3.select("#varnish");
+
+    var svg = varnish
+        .append("svg")
+        .attr("width", imgBounds.width)
+        .attr("height", imgBounds.height);
+    data.forEach(function(d) {
+        let pos = `coordinate-${d.x}-${d.y}`;
+        let observationId = `#ob-${pos}`;
+		d.xOff = d.xOff == undefined ? 0: d.xOff;
+		d.yOff = d.yOff == undefined ? 0: d.yOff;
+        varnish.append('div')
+            .attr("id", `ob-${pos}`)
+            .attr('class', 'observation')
+            .attr('left', d.x)
+            .attr('top', d.y)
+            .html(`<h2>${d.textHeader}</h2><p style="font-weight:normal;margin-top:10px">${d.text}</p>`)
+        svg.append("svg:image")
+            .attr("id", pos)
+            .attr("class", `coordinate ${d.dimensionType}`)
+            .attr("x", d.x)
+            .attr("y", d.y)
+            .attr("title", d.text)
+            .attr("xlink:href", `./${d.dimensionType}.svg`)
+            .on("mouseover", function() {
+                d3.select(observationId)
+                    .style("visibility", "visible")
+                    .style("left", `${d3.event.clientX}px`)
+                    .style("top", `${d3.event.clientY}px`)
+                d3.selectAll(`#${d.dimension}`).style("visibility", "visible");
+            })
+            .on("mouseout", function() {
+                d3.select(observationId).style("visibility", "hidden");
+                d3.selectAll(`#${d.dimension}`).style("visibility", "hidden");
+            });
+        addDimension(svg, d);
+    });
+
+});
