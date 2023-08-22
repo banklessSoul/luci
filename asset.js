10ccd9367f746d2420c1a2489eff27c7370da9dd
banklessSoul <142628476+banklessSoul@users.noreply.github.com>
2023-08-22 13:00:03 -0700

    Update asset.js

--- asset.js
+++ asset.js
@@ -39,39 +39,86 @@ function addPath(svg, d) {
         .attr("fill", d.color);
 }
 
-d3.json("observations.json", function(error, data) {
-    var imgBounds = d3.select('#mg').node().getBoundingClientRect();
-    var varnish = d3.select("#varnish");
+function monumentGame() {
+	d3.json("observations.json", function(error, data) {
+		var imgBounds = d3.select('#mg').node().getBoundingClientRect();
+		var varnish = d3.select("#varnish");
 
-    var svg = varnish
-        .append("svg")
-        .attr("width", imgBounds.width)
-        .attr("height", imgBounds.height);
-    data.forEach(function(d) {
-        let pos = `coordinate-${d.x}-${d.y}`;
-        let observationId = `ob-${pos}`;
-	d.xOff = d.xOff == undefined ? 0: d.xOff;
-	d.yOff = d.yOff == undefined ? 0: d.yOff;
-        varnish.append('div')
-            .attr("id", observationId)
-            .attr('class', 'observation')
-            .attr('left', `${d.x}px`)
-            .attr('top', `${d.y}px`)
-           .html(`<h2>${d.textHeader}<span style="float:right;font-weight:normal;cursor:pointer;" onclick=\"closeObservation('${observationId}', '${d.dimension}')\">x</span></h2><p style="font-weight:normal;margin-top:10px">${d.text}</p>`);
-        svg.append("svg:image")
-            .attr("id", pos)
-            .attr("class", `coordinate ${d.dimensionType}`)
-            .attr("x", d.x)
-            .attr("y", d.y)
-            .attr("title", d.text)
-            .attr("xlink:href", `./${d.dimensionType}.svg`)
-            .on("click", function() {
-                d3.select(`#${observationId}`)
-                    .style("visibility", "visible")
-                    .style("left", `${window.pageXOffset + d3.event.clientX}px`)
-                    .style("top", `${window.pageYOffset + d3.event.clientY}px`)
-                d3.selectAll(`#${d.dimension}`).style("visibility", "visible");
-            });
-        addDimension(svg, d);
-    });
-});
+		var svg = varnish
+			.append("svg")
+			.attr("width", imgBounds.width)
+			.attr("height", imgBounds.height);
+		data.forEach(function(d) {
+			let pos = `coordinate-${d.x}-${d.y}`;
+			let observationId = `ob-${pos}`;
+		d.xOff = d.xOff == undefined ? 0: d.xOff;
+		d.yOff = d.yOff == undefined ? 0: d.yOff;
+			varnish.append('div')
+				.attr("id", observationId)
+				.attr('class', 'observation')
+				.attr('left', `${d.x}px`)
+				.attr('top', `${d.y}px`)
+			   .html(`<h2>${d.textHeader}<span style="float:right;font-weight:normal;cursor:pointer;" onclick=\"closeObservation('${observationId}', '${d.dimension}')\">x</span></h2><p style="font-weight:normal;margin-top:10px">${d.text}</p>`);
+			svg.append("svg:image")
+				.attr("id", pos)
+				.attr("class", `coordinate ${d.dimensionType}`)
+				.attr("x", d.x)
+				.attr("y", d.y)
+				.attr("title", d.text)
+				.attr("xlink:href", `./${d.dimensionType}.svg`)
+				.on("click", function() {
+					d3.select(`#${observationId}`)
+						.style("visibility", "visible")
+						.style("left", `${window.pageXOffset + d3.event.clientX}px`)
+						.style("top", `${window.pageYOffset + d3.event.clientY}px`)
+					d3.selectAll(`#${d.dimension}`).style("visibility", "visible");
+				});
+			addDimension(svg, d);
+		});
+	});
+}
+
+function lineage() {
+	var imgBounds = d3.select('#council').node().getBoundingClientRect();
+	var w = imgBounds.width,
+      h = imgBounds.height,
+      start = .5,
+      end = 1.3,
+      numSpirals = 2;
+
+    var theta = function(r) {
+      return numSpirals * Math.PI * r;
+    };
+	
+	var svg = d3.select("#lineage").append("svg")
+		  .attr("width", w)
+		  .attr("height", h)
+		  .append("g")
+		  .attr("transform", "translate(" + w / 2 + "," + h / 2 + ")");
+	
+	for (var i=0; i<=2; i++) {
+		 var r = d3.min([w, h]) / 1.5;
+
+		var radius = d3.scaleLinear()
+		  .domain([start, end])
+		  .range([50, r]);		
+
+		var points = d3.range(start, end + 0.001, (end - start) / 1000);
+
+		var spiral = d3.radialLine()
+		  .curve(d3.curveCardinal)
+		  .angle(theta)
+		  .radius(radius);
+
+		var path = svg.append("path")
+		  .datum(points)
+		  .attr("id", "spiral")
+		  .attr("class", "lineage")
+		  .attr("d", spiral)
+		  ;
+		  
+		start +=.65
+		end += .65
+	}
+   
+}
